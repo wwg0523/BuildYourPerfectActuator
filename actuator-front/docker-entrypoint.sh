22e7b5cd85ead14e2a4c691225dc4bd00f327a63
wwg0523 <wwg0523@hanmail.net>
2025-11-10 03:29:53 +0900

    refactor: Nginx 설정 단순화 (하드코딩)
    - docker-entrypoint.sh 간단히 (envsubst 제거)
    - nginx.conf: 포트 5005 하드코딩
    - nginx.conf: 백엔드 주소 actuator-back:4004 하드코딩

--- actuator-front/docker-entrypoint.sh
+++ actuator-front/docker-entrypoint.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
-# Nginx 설정의 환경변수를 치환
-
-# 기본값 설정
-export FRONTEND_PORT=${FRONTEND_PORT:-5005}
-export BACKEND_HOST=${BACKEND_HOST:-actuator-back}
-export BACKEND_PORT=${BACKEND_PORT:-4004}
-
-# envsubst를 사용하여 환경변수 치환
-envsubst < /etc/nginx/conf.d/default.conf > /tmp/default.conf
-mv /tmp/default.conf /etc/nginx/conf.d/default.conf
-
 # Nginx 시작
+
 exec nginx -g "daemon off;"
